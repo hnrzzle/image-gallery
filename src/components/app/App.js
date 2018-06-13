0c5f343b2e7ba7fca6e10856c992b494eaa37f39
victorbofill <victorm.bofill@gmail.com>
2018-06-13 16:40:11 -0700

    initialization

---
+++ src/components/app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>I'm an app!</h1>
+      </main>
+    );
+  }
+}
---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { images, card } from './components/game/reducers';
+
+const rootReducer = combineReducers({
+  images,
+  card
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk
+    )
+  )
+);
+
+export default store;
