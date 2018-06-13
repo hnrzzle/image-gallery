7f49251db393975c3df808b7f3c75280cebf20cb
victorbofill <victorm.bofill@gmail.com>
2018-06-13 16:58:14 -0700

    re-arranged the base infrastructure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Main from './Main';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import styles from '../../main.css';
+
+import Home from './Home';
 
 export default class App extends Component {
   render() {
     return (
-      <main>
-        <Header />
-        <Main />
-        <Footer />
-      </main>
+      <Router>
+        <div>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Redirect to="/"/>
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
