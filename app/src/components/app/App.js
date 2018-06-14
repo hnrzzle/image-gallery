ecb82c4914393c9219f6857d41de35384dede0d3
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-14 10:06:37 -0700

    basic styling

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import styles from '../../main.css';
-
+import Nav from './Nav';
 import Home from './Home';
+import Header from './Header';
+import styles from '../../main.css';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <main>
-            <Switch>
-              <Route exact path="/" component={Home}/>
-              <Redirect to="/"/>
-            </Switch>
-          </main>
+        <div className="app">
+          <Nav/>
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Redirect to="/"/>
+          </Switch>
+          <footer>
+            <h3>{'I\'m the footer!'}</h3>
+          </footer>
         </div>
       </Router>
     );
---
+++ app/src/components/app/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  
+  render() {
+    return (
+      <header>
+        <h2>Header</h2>
+      </header>
+    );
+  }
+}
