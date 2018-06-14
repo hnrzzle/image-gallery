d898088f1d53bb492827c67b7929fe9be06f9ce6
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-14 11:28:10 -0700

    add albums component and style

--- app/src/components/albums/Albums.js
+++ app/src/components/albums/Albums.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Album from './Album';
+import styles from './Albums.css';
 
 export default class Albums extends Component {
   render() {
@@ -7,7 +8,7 @@ export default class Albums extends Component {
     // const { albums } = this.props;
 
     return (
-      <ul>
+      <ul className={styles.albums}>
         <Album/>
         {/* {albums.map((album, i) => (
           <Album key={i} album={album}/>
