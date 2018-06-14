5a24ff9c178649d07b97f7108acef168a9f04c1a
victorbofill <victorm.bofill@gmail.com>
2018-06-14 11:00:52 -0700

    updated index.js store path

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/app/App';
-import store from './store';
+import store from './store/store';
 import './main.css';
 
 ReactDOM.render(
