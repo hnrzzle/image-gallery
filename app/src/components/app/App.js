6548d1cfab7eb9c50fe8a1ef1395a308fdb5d77d
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-14 11:00:39 -0700

    working on front end

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import Nav from './Nav';
 import Home from './Home';
 import Header from './Header';
+import Albums from '../albums/Albums';
 import styles from '../../main.css';
 
 export default class App extends Component {
@@ -12,13 +13,15 @@ export default class App extends Component {
         <div className="app">
           <Nav/>
           <Header/>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            {/* <Route exact path="/albums" component={Albums}/>
-            <Route exact path="/images" component={Images}/>
-            <Route exact path="/albums/new" component={NewAlbum}/> */}
-            <Redirect to="/"/>
-          </Switch>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/albums" component={Albums}/>
+              {/*<Route exact path="/images" component={Images}/>
+              <Route exact path="/albums/new" component={NewAlbum}/> */}
+              <Redirect to="/"/>
+            </Switch>
+          </main>
           <footer>
             <h3>{'I\'m the footer!'}</h3>
           </footer>
--- app/src/components/app/Home.js
+++ app/src/components/app/Home.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import styles from '../../main.css';
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <main>
-        <h1>{'Look at me, I\'m the home screen!'}</h1>
-      </main>
+      <h1>{'Look at me, I\'m the home screen!'}</h1>
     );
   }
 }
