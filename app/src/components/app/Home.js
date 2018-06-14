83dc53ea5d03dc8e7ab61037cc1127c3346e6743
victorbofill <victorm.bofill@gmail.com>
2018-06-14 11:00:44 -0700

    removed main tag from home component

--- app/src/components/app/Home.js
+++ app/src/components/app/Home.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <main>
-        <h1>{'Look at me, I\'m the home screen!'}</h1>
-      </main>
+      <h1>{'Look at me, I\'m the home screen!'}</h1>
     );
   }
 }
