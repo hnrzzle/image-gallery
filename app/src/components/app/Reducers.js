c4efc34a345f7e6b7d658fbeffd710d630330160
victorbofill <victorm.bofill@gmail.com>
2018-06-14 10:39:04 -0700

    moved store docs and created middleware

---
+++ app/src/components/app/Reducers.js
@@ -0,0 +1,14 @@
+export const LOADING_START = 'LOADING_START';
+export const LOADING_END = 'LOADING_END';
+export const ERROR = 'ERROR';
+
+export function loading(state = false, { type }) {
+  switch (type) {
+    default:
+      return state;
+  }
+}
+
+// export function error(state = null, { type, payload }) {
+  
+// }
