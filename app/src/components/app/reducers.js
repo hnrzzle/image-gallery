82ed0f6d7f065f2ef474e8a088a4a33ac7b5edf4
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-15 12:06:33 -0700

    start reducers and reducers test

--- app/src/components/app/reducers.js
+++ app/src/components/app/reducers.js
@@ -1,3 +1,5 @@
+export const ALBUMS_LOAD = 'ALBUMS_LOAD';
+export const ALBUM_LOAD = 'ALBUM_LOAD';
 export const LOADING_START = 'LOADING_START';
 export const LOADING_END = 'LOADING_END';
 export const ERROR = 'ERROR';
---
+++ app/src/components/app/reducers.test.js
@@ -0,0 +1,4 @@
+import {
+  ALBUMS_LOAD, ALBUM_LOAD, LOADING_START, LOADING_END, ERROR
+} from './reducers';
+
