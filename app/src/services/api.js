780e9dd1d0e7d2d7b46e26e17be2e18f6e2daf87
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-15 11:55:20 -0700

    write api call

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,3 +1,7 @@
 import { get } from './request';
 
 const URL = 'mongodb://localhost:27017/images';
+
+
+export const getAllAlbums = () => get(URL);
+export const getAlbum = id => get(`${URL}/${id}`);
