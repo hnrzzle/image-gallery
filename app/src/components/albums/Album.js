3b61e0d45679eb50bd6031e176c268abdef9417d
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-14 17:55:37 -0700

    start album gallery

--- app/src/components/albums/Album.js
+++ app/src/components/albums/Album.js
@@ -6,7 +6,7 @@ export default class Album extends Component {
   render() {
     return (
       <div>
-        <h3>Album Title</h3>
+        <h3>Albums</h3>
         <li className={styles.album}>
           <Link to="/albums/:id">
             <img className="posterimage" src="http://support.yumpu.com/en/wp-content/themes/qaengine/img/default-thumbnail.jpg"/>
