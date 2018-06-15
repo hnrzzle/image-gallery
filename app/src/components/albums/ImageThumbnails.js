b56cc3cab5d25cd8918102405f2c6e5f2296ae4c
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-15 10:03:28 -0700

    changes to create new head

--- app/src/components/albums/ImageThumbnails.js
+++ app/src/components/albums/ImageThumbnails.js
@@ -12,6 +12,8 @@ export default class ImageThumbnails extends Component {
           <Thumbnail/>
           <Thumbnail/>
           <Thumbnail/>
+          <Thumbnail/>
+          <Thumbnail/>
         </ul>
       </Fragment>
     );
