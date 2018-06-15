300cf9f87d0c5b5d075dc7d656db33794c96121d
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-15 11:48:09 -0700

    changes to api

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,7 +1,3 @@
 import { get } from './request';
 
 const URL = 'mongodb://localhost:27017/images';
-const PETS_URL = `${URL}/pets`;
-
-export const getAllPets = () => get(PETS_URL);
-export const getPet = id => get(`${PETS_URL}/${id}`);
