2451c255c1140e2f86d2923a74ff15512f53474c
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-15 09:15:06 -0700

    album thumbnail view

--- app/src/components/albums/AlbumThumbnails.js
+++ app/src/components/albums/AlbumThumbnails.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Thumbnail from './Thumbnail';
+import styles from './AlbumThumbnails.css';
 
 export default class AlbumThumbnails extends Component {
   
   render() {
     return (
-      <h3>Thumbnail View</h3>
+      <Fragment>
+        <h3>Thumbnail View</h3>
+        <ul className={styles.thumbnails}>
+          <Thumbnail/>
+          <Thumbnail/>
+          <Thumbnail/>
+        </ul>
+      </Fragment>
     );
   }
 }
