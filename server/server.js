d8d8e365f2b2e02e35830572bb6166fb2cef5859
victorbofill <victorm.bofill@gmail.com>
2018-06-14 18:04:32 -0700

    plenty of progress

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/connect');
@@ -8,6 +9,6 @@ const server = http.createServer(app);
 const port = process.env.port || 3000;
 
 server.listen(port, () => {
-    // eslint-disable-next-line
-    console.log('server running on', server.address().port);
+  // eslint-disable-next-line
+  console.log('server running on', server.address().port);
 });
