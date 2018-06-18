41c8e39a0c1e6324f61b2ca31d157321b581d896
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-18 07:45:16 -0700

    albums reducers passing

--- app/src/components/albums/reducers.test.js
+++ app/src/components/albums/reducers.test.js
@@ -7,4 +7,29 @@ describe('Album reducers', () => {
     expect(state).toEqual([]);
   });
 
+  it('loads albums', () => {
+    const album = {
+      title: 'A Scenic Beach',
+      description: 'A beach...',
+      posterImage: 'https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg'
+    };
+    const state = albums(undefined, { type: ALBUMS_LOAD, payload: [album] });
+    expect(state).toEqual([album]);
+  });
+  
+  it('adds album', () => {
+    const album = {
+      title: 'A Scenic Beach',
+      description: 'A beach...',
+      posterImage: 'https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg'
+    };
+    const album2 = {
+      title: 'A very Scenic Beach',
+      description: 'A beach... scenic',
+      posterImage: 'google.com/beach'
+    };
+    const state = albums([album], { type: ALBUM_ADD, payload: album2 });
+    expect(state).toEqual([album, album2]);
+  });
+
 });
