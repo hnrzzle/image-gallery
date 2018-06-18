966a2a2107fbb32cd14da7f83cfc2d3eafac0672
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-18 07:51:16 -0700

    reducers passing

--- app/src/components/albums/reducers.js
+++ app/src/components/albums/reducers.js
@@ -1,5 +1,7 @@
 export const ALBUMS_LOAD = 'ALBUMS_LOAD';
 export const ALBUM_ADD = 'ALBUM_ADD';
+export const IMAGES_LOAD = 'IMAGES_LOAD';
+export const IMAGE_ADD = 'IMAGE_ADD';
 
 
 export const getAlbums = state => state.albums;
@@ -8,6 +10,17 @@ export function albums(state = [], { type, payload }) {
     case ALBUMS_LOAD: 
       return payload;
     case ALBUM_ADD:
+      return [...state, payload];
+    default:
+      return state;
+  }
+}
+
+export function images(state = [], { type, payload }) {
+  switch (type) {
+    case IMAGES_LOAD: 
+      return payload;
+    case IMAGE_ADD:
       return [...state, payload];
     default:
       return state;
--- app/src/components/albums/reducers.test.js
+++ app/src/components/albums/reducers.test.js
@@ -1,4 +1,4 @@
-import { albums, ALBUMS_LOAD, ALBUM_ADD } from './reducers';
+import { albums, ALBUMS_LOAD, ALBUM_ADD, images, IMAGE_ADD, IMAGES_LOAD } from './reducers';
 
 describe('Album reducers', () => {
 
@@ -6,7 +6,7 @@ describe('Album reducers', () => {
     const state = albums(undefined, {});
     expect(state).toEqual([]);
   });
-
+  
   it('loads albums', () => {
     const album = {
       title: 'A Scenic Beach',
@@ -30,6 +30,43 @@ describe('Album reducers', () => {
     };
     const state = albums([album], { type: ALBUM_ADD, payload: album2 });
     expect(state).toEqual([album, album2]);
+  });
+  
+});
+
+describe('images reducers', () => {
+  
+  it('returns empty array for default state', () => {
+    const state = images(undefined, {});
+    expect(state).toEqual([]);
+  });
+  
+  it('loads images', () => {
+    const image = {
+      title: 'A Scenic Beach',
+      description: 'A beach...',
+      url: 'https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg',
+      albumId: '4'
+    };
+    const state = images(undefined, { type: IMAGES_LOAD, payload: [image] });
+    expect(state).toEqual([image]);
+  });
+  
+  it('adds image', () => {
+    const image = {
+      title: 'A Scenic Beach',
+      description: 'A beach...',
+      url: 'https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg',
+      albumId: '4'
+    };
+    const image2 = {
+      title: 'A very Scenic Beach',
+      description: 'A beach... scenic',
+      url: 'google.com/beach',
+      albumId: '4'
+    };
+    const state = images([image], { type: IMAGE_ADD, payload: image2 });
+    expect(state).toEqual([image, image2]);
   });
 
 });
