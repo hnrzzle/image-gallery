b9d8099b29796262d313f8bc5145850245393afa
victorbofill <victorm.bofill@gmail.com>
2018-06-19 13:47:44 -0700

    re-wrote promise-mw

--- app/src/store/promise-mw.js
+++ app/src/store/promise-mw.js
@@ -2,29 +2,28 @@ import { LOADING_START, LOADING_END, ERROR } from '../components/app/reducers';
 
 const isPromise = val => val && typeof val.then === 'function';
 
-export const promiseMiddleware = ({ dispatch }) => next => action => {
+export default ({ dispatch }) => next => action => {
 
   const { type, payload } = action;
   if(!isPromise(payload)) return next(action);
 
   dispatch({ type: LOADING_START });
 
-  payload
+  return payload
     .then(
       result => {
-        dispatch({
+        dispatch({ type: LOADING_END });
+
+        return dispatch({
           type,
           payload: result
         });
+
       },
       err => {
-        dispatch({
-          type: ERROR,
-          payload: err
-        });
+        dispatch({ type: LOADING_END });
+        dispatch({ type: ERROR, payload: err });
+        throw err;
       }
-    )
-    .then(() => {
-      dispatch({ type: LOADING_END });
-    });
+    );
 };
