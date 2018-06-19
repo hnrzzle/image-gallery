673666346ace1bf300d6864c989afba6fd85f3a3
victorbofill <victorm.bofill@gmail.com>
2018-06-19 09:28:37 -0700

    added api services

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,9 +1,19 @@
 import { get, post } from './request';
 
 const URL = 'api/albums';
+const AUTH_URL = `${URL}/auth`;
 
 export const getAllAlbums = () => get(URL);
 export const getAlbum = id => get(`${URL}/${id}`);
 export const postAlbum = album => post(URL, album);
 export const getImages = id => get(`${URL}/${id}/images`);
 export const postImage = (image, albumId) => post(`${URL}/${albumId}/images`, image);
+
+export const signin = credentials => post(`${AUTH_URL}/signin`, credentials);
+export const signup = credentials => post(`${AUTH_URL}/signup`, credentials);
+
+export const verifyUser = token => get(`${AUTH_URL}/verify`, {
+  headers: {
+    Authorization: token
+  }
+});
