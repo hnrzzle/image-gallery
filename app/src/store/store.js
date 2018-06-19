2205557425a1d2fd2ee21c7ad4a473af3b6ac621
victorbofill <victorm.bofill@gmail.com>
2018-06-19 14:16:00 -0700

    added all reducers to store

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { albums, images } from '../../src/components/albums/reducers';
 import { user, checkedAuth } from '../../src/components/auth/reducers';
+import { loading } from '../components/app/reducers';
 import promiseMiddleware from './promise-mw';
 
 const rootReducer = combineReducers({
   albums,
   images,
   user,
-  checkedAuth
+  checkedAuth,
+  loading
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
