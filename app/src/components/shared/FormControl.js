19db14a59f6dba910f261512be9065ac8e9ebdc6
victorbofill <victorm.bofill@gmail.com>
2018-06-19 09:47:48 -0700

    created FormControl

---
+++ app/src/components/shared/FormControl.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class FormControl extends PureComponent {
+  static propTypes = {
+    label: PropTypes.string,
+    children: PropTypes.element
+  };
+
+  render() {
+    const { label, children } = this.props;
+
+    return (
+      <div>
+        { label && <label>{label}:</label>}
+        <div>
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FormControl;
