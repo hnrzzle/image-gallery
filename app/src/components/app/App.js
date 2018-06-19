3ae9d9f014a3fa7f89c1311ec3ffcb25dc472285
victorbofill <victorm.bofill@gmail.com>
2018-06-19 10:29:04 -0700

    added auth functionality to app.js

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import PrivateRoute from './PrivateRoute';
+import { tryLoadUser } from '../auth/actions';
+import { getCheckedAuth } from '../auth/reducers';
+
 import Nav from './Nav';
 import Home from './Home';
 import Header from './Header';
@@ -8,25 +13,39 @@ import Images from '../images/Images';
 import Albums from '../albums/Albums';
 import AlbumDetail from '../albums/AlbumDetail';
 import NewAlbum from '../albums/NewAlbum';
+import { connect } from 'net';
 
-export default class App extends Component {
+class App extends PureComponent {
+  static propTypes = {
+    tryLoadUser: PropTypes.func.isRequired,
+    checkedAuth: PropTypes.bool.isRequired,
+  };
+
+  componentDidMount() {
+    this.props.tryLoadUser();
+  }
+
   render() {
+    const { checkedAuth } = this.props;
+    
     return (
       <Router>
         <div className="app">
           <Nav/>
           <Header/>
           <main>
+            { checkedAuth &&
             <Switch>
               <Route exact path="/" component={Home}/>
 
               <Route exact path="/albums" component={Albums}/>
-              <Route exact path="/albums/new" component={NewAlbum}/>
-              <Route path="/albums/:id" component={AlbumDetail} />
+              <PrivateRoute exact path="/albums/new" component={NewAlbum}/>
+              <PrivateRoute path="/albums/:id" component={AlbumDetail} />
 
               <Route exact path="/images" component={Images}/>
               <Redirect to="/"/>
             </Switch>
+            }
           </main>
           <footer>
             <h3>{'Footer'}</h3>
@@ -37,3 +56,7 @@ export default class App extends Component {
   }
 }
 
+export default connect(
+  state => ({ checkedAuth: getCheckedAuth(state) }),
+  { tryLoadUser }
+)(App);
