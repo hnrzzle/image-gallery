adfef2014215ef71ad01aa9e1d4136bc6c149c97
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-19 15:29:23 -0700

    made adding a new album a protected route

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { tryLoadUser } from '../auth/actions';
+import { getCheckedAuth } from '../auth/reducers';
+import PropTypes from 'prop-types';
+import PrivateRoute from './PrivateRoute';
 import Nav from './Nav';
 import Home from './Home';
 import Header from './Header';
@@ -10,7 +15,17 @@ import Albums from '../albums/Albums';
 import AlbumDetail from '../albums/AlbumDetail';
 import NewAlbum from '../albums/NewAlbum';
 
-export default class App extends Component {
+class App extends Component {
+
+  static propTypes = {
+    tryLoadUser: PropTypes.func.isRequired,
+    checkedAuth: PropTypes.bool.isRequired
+  };
+
+  componentDidMount() {
+    this.props.tryLoadUser();
+  }
+
   render() {
     return (
       <Router>
@@ -22,7 +37,7 @@ export default class App extends Component {
               <Route exact path="/" component={Home}/>
 
               <Route exact path="/albums" component={Albums}/>
-              <Route exact path="/albums/new" component={NewAlbum}/>
+              <PrivateRoute exact path="/albums/new" component={NewAlbum}/>
               <Route path="/albums/:id" component={AlbumDetail} />
               <Route path="/auth" component={Auth}/>
               <Route exact path="/images" component={Images}/>
@@ -38,3 +53,7 @@ export default class App extends Component {
   }
 }
 
+export default connect(
+  state => ({ checkedAuth: getCheckedAuth(state) }),
+  { tryLoadUser }
+)(App);
