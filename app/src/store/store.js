592b95eb98ba9d9cf64df6dead3d746d8025d043
victorbofill <victorm.bofill@gmail.com>
2018-06-19 13:47:50 -0700

    added promise mw to store

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { albums, images } from '../../src/components/albums/reducers';
 import { user, checkedAuth } from '../../src/components/auth/reducers';
-import { promiseMiddleware } from './promise-mw';
+import promiseMiddleware from './promise-mw';
 
 const rootReducer = combineReducers({
   albums,
