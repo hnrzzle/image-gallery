6b43d139a362ee1bd542afcddab13f952add87f3
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-21 10:11:42 -0700

    basic image gallery complete

--- app/src/components/albums/ImageGallery.js
+++ app/src/components/albums/ImageGallery.js
@@ -1,16 +1,41 @@
 import React, { Component, Fragment } from 'react';
+import { getImages } from './reducers';
+import { loadImages } from './actions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class ImageGallery extends Component {
+class ImageGallery extends Component {
 
+  static propTypes = {
+    albumId: PropTypes.string.isRequired,
+    images: PropTypes.array,
+    loadImages: PropTypes.func.isRequired
+  };
+  
+  componentDidMount() {
+    this.props.loadImages(this.props.albumId);
+  }
   
   render() {
+    const { images } = this.props;
+
     return (
       <Fragment>
         <h2>Album Title</h2>
         <h3>Gallery View</h3>
-        <img src="http://support.yumpu.com/en/wp-content/themes/qaengine/img/default-thumbnail.jpg" />
-        <p>Description</p>
+        {images.map((image, i) => (
+          <Fragment  key={i}>
+            <h4 className="title">{image.title}</h4>
+            <img src={image.url} className="url" />
+            <span className="description">{image.description}</span>
+          </Fragment>
+        ))}
       </Fragment>
     );
   }
 }
+
+export default connect(
+  state => ({ images: getImages(state) }),
+  { loadImages }
+)(ImageGallery);
