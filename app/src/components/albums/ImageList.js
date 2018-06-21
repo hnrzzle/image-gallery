902c22573bf34a5bf67d64f977687ee3f2f8d496
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-21 09:27:11 -0700

    image list details rendering, need to style using grid

--- app/src/components/albums/ImageList.js
+++ app/src/components/albums/ImageList.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { getImages } from './reducers';
 import { loadImages } from './actions';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import styles from './ImageList';
 
 class ImageList extends Component {
 
@@ -12,10 +13,29 @@ class ImageList extends Component {
     loadImages: PropTypes.func.isRequired
   };
 
+  componentDidMount() {
+    this.props.loadImages(this.props.albumId);
+  }
+
   render() {
+    const { images } = this.props;
 
     return (
-      <h3>List View</h3>      
+      <Fragment>
+        <h3>Image Details</h3>
+        <div className={styles.list}>
+          <h4 className="title-header">Title</h4>
+          <h4 className="description-header">Description</h4>
+          <h4 className="url-header">URL</h4>
+          {images.map((image, i) => (
+            <Fragment  key={i}>
+              <h4 className="title">{image.title}</h4>
+              <span className="description">{image.description}</span>
+              <span className="url">{image.url}</span>
+            </Fragment>
+          ))}
+        </div>
+      </Fragment>
     );
   }
 }
